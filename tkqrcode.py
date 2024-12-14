bb67c3d489a8a64aa3fd22a5daf88645b898b82c
dmtzs <diego.mtzs00@gmail.com>
2024-12-14 12:53:17 -0600

    Adding few updates
    Defined versions of libraries used and using snake case in main file

--- tkqrcode.py
+++ tkqrcode.py
@@ -1,17 +1,17 @@
 try:
     from biblios import *
-except ImportError as eImp:
-    print(f"The following ERROR ocurred: {eImp}")
+except ImportError as err_imp:
+    print(f"The following ERROR ocurred: {err_imp}")
 
 if __name__== "__main__":
-    mainTitleApp= "QR codes"# Label title inside the frame of the application
-    titleApp= "QR Code generator"# Title inside the application bar
+    main_title_app = "QR codes"  # Label title inside the frame of the application
+    title_app = "QR Code generator"  # Title inside the application bar
 
     try:
-        met= metodos.funciones(titleApp, mainTitleApp)
+        met= metodos.funciones(title_app, main_title_app)
         met.GUI()
-    except Exception as ex:
-        print(f"The following ERROR ocurred: {ex}")
+    except Exception as exc:
+        print(f"The following ERROR ocurred: {exc}")
     except KeyboardInterrupt:
         print("Was pressed Ctrl + C")
     finally:
