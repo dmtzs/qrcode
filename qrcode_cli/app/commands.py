8b8bfb49ad3d1c4c2c1e210268b5a5d97ba0dc01
dmtzs <diego.mtzs00@gmail.com>
2025-01-06 14:41:15 -0600

    Skeleton cli
    Adding a skeletoin of the cli of the qr code generator

---
+++ qrcode_cli/app/commands.py
@@ -0,0 +1,13 @@
+from app import app
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
