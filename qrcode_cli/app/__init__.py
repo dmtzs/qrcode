94f96f99cef177243c47477fd6b07d67d0be206c
dmtzs <diego.mtzs00@gmail.com>
2025-01-06 15:18:37 -0600

    Update cli for qrcode
    With the CLI of the qrcode now we can generate a wifi qrcode

--- qrcode_cli/app/__init__.py
+++ qrcode_cli/app/__init__.py
@@ -1,3 +1,5 @@
 import typer
 
 app = typer.Typer()
+
+from app import commands
--- qrcode_cli/app/commands.py
+++ qrcode_cli/app/commands.py
@@ -1,13 +1,34 @@
+import qrcode
 from app import app
-
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+from typer import Option
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
+def wifiqr(
+    ssid: str = Option(..., help="The SSID of the network."),
+    password: str = Option(..., help="The password of the network."),
+    encryption: str = Option("WPA", help="The encryption type of the network."),
+    hidden: bool = Option(False, help="Whether the network is hidden."),
+    ascii: bool = Option(False, help="Print the QR code as ASCII in terminal, otherwise an image will be saved."),
+) -> None:
+    """
+    Generate a QR code for a WiFi network.
+
+    The QR code will contain the SSID, password, encryption, and hidden status.
+
+    Args:
+        - ssid (str): The SSID of the network.
+        - password (str): The password of the network.
+        - encryption (str): The encryption type of the network.
+        - hidden (bool): Whether the network is hidden.
+    """
+    wifi_data = f"WIFI:S:{ssid};T:{encryption};P:{password};H:{int(hidden)};;"
+    qr = qrcode.QRCode()
+    qr.add_data(wifi_data)
+    qr.make()
+    if ascii:
+        qr.print_ascii(invert=True)
     else:
-        print(f"Bye {name}!")
+        qr.make_image(fill_color="black", back_color="white").save("wifi.png")
+
+# comando: python main.py wifiqr --help
